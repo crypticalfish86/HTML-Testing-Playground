d621185d73f51a07e479ed1f47235b3b1755d487
unknown <jasonweerawardena@gmail.com>
2023-04-14 18:19:36 +0100

    fade

--- fade/src/App.js
+++ fade/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { animated, useSpring } from "@react-spring/web"
 function App() {
+
+  const emergeDiv = useSpring
+  (
+    {from: {opacity: 0.05}, to: {opacity: 1}}
+  )
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <animated.div style={emergeDiv}>
+        <div id="faded_div">
+          <div>fade in and out</div>
+        </div>
+      </animated.div>
     </div>
   );
 }
