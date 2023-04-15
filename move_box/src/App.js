f793c8fbb93601f21ab6b2da6f6cdddb27cce690
unknown <jasonweerawardena@gmail.com>
2023-04-15 17:56:03 +0100

    move_box

---
+++ move_box/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id='blue_box_parent_1'>
+        <div id='red_box_child_1'> jagged turn to center</div>
+      </div>
+      <div id='blue_box_parent_2'>
+        <div id='red_box_child_2A'>smooth turn to right</div>
+        <div id='red_box_child_2B'>smooth turn 2 rem to the right</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
