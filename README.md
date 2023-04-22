969109e69ff7365e6d92898201abc059d113af4d
unknown <jasonweerawardena@gmail.com>
2023-04-22 18:57:40 +0100

    expandable text box complete

--- README.md
+++ README.md
@@ -2,10 +2,16 @@ This is a personal repository to test various styling and animations in the reac
 and will be used by me in the future as a reference for other web projects.
 
 1. Fade
-    a simple fade in of a div on page load
+    a simple fade in of a div on page load, attempt at using react-spring (most likely come back to this dependency, figuring out base animation 
+    first)
     dependencies: react, react-spring.
 
 2. move_box
     a simple transition and moving of a box sized div over the page, done with several different
     speeds using the translation property in CSS
+    dependencies: react
+
+3. expandable_text_box
+    This was an issue with animation that was a large problem for me for a while but now i have figured out how to properly expand a box with actual
+    content inside using smooth animation.
     dependencies: react
--- expandable_text_box/src/App.js
+++ expandable_text_box/src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
 import bee from './assets/bee.jpg';
+import { useState } from 'react';
 
 function App() {
   
-  function expandBox(event)
+  const [expandCollapseBox, setBox] = useState(null);
+
+  function expandBox(event) 
   {
     event.preventDefault();
+    if(expandCollapseBox) //this function checks if expandbox was already applied to it and if it was cancels the expansion
+    {
+      setBox(null)
+    }else
+    {
+      const text = document.getElementsByClassName('Box_Text');
+      setBox('Box_Text_Expanded')  //applies a class which expands the size of the text box
+    }
   }
 
   return (
     <div className="App">
-      <div id="Entire_Box">
-        <div id="Box_Title">Title</div>
-        <div id="Box_Body">
-          <img src={bee} id="image"/>
-          <div id="Box_Text">
+      <div id={expandCollapseBox} className="Entire_Box">
+        <div className="Box_Title">Title</div>
+        <div className="Box_Body">
+          <div>
+            <img src={bee} className="image" alt=''/>
+            <button onClick={(event) => {expandBox(event)}}>Read More</button>
+          </div>
+          <div className='Box_Text'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia vitae mauris et pretium. Sed eget mollis augue. Donec porta, lacus non varius pellentesque, turpis velit tristique diam, eu fermentum est augue vitae est. Curabitur eleifend velit id leo pellentesque malesuada. Etiam eu sem leo. Sed id tellus eleifend, tincidunt risus rutrum, sollicitudin enim. In hac habitasse platea dictumst. Morbi id odio gravida, interdum enim id, vulputate purus. Mauris sapien dolor, maximus ut felis ut, sodales convallis sapien. Praesent aliquam urna placerat leo gravida sollicitudin. Ut iaculis orci id turpis auctor tristique.
 
             Donec dictum molestie ipsum eget tincidunt. Pellentesque justo erat, dapibus a tincidunt in, sollicitudin eget ex. Maecenas ac nisl nec diam commodo sollicitudin. Aliquam ornare iaculis est, vulputate consectetur augue lobortis eleifend. Duis leo odio, posuere eget dui sit amet, mollis hendrerit metus. Ut varius viverra erat eu blandit. Integer sed quam sed lectus sagittis semper et et arcu. In vel elit dignissim, dignissim ante eu, volutpat risus.
@@ -22,7 +36,6 @@ function App() {
             Suspendisse tempus tellus at tortor ultrices ullamcorper. Donec eu felis ornare, lacinia tellus id, placerat justo. Vestibulum gravida magna sed tellus tincidunt ornare non sed massa. Integer commodo id leo non porttitor. Fusce venenatis risus in semper faucibus. Ut sagittis. 
           </div>
         </div>
-        <button onClick={(event) => {expandBox(event)}}>Read More</button>
       </div>
     </div>
   );
