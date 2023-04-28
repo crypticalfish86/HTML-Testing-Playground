a8c59815656faf64b8302bfbac997a445ea818e5
unknown <jasonweerawardena@gmail.com>
2023-04-28 17:00:09 +0100

    getBoundingClientRect() information

--- 7_bounding_client_rect_box_transition/src/App.js
+++ 7_bounding_client_rect_box_transition/src/App.js
@@ -22,3 +22,29 @@ function App() {
 }
 
 export default App;
+
+/*
+INFORMATION on getBoundingClientRect();
+
+getBoundingClientRect() is a method that returns the size of an element and its position relative to the viewport. It returns an object with properties top, right, bottom, left, x, y, width, and height.
+
+To use it, you can call getBoundingClientRect() on the element you want to get the size and position of, like this:
+
+const boxRef = useRef(null);
+const boxRect = boxRef.current.getBoundingClientRect();
+
+This will give you an object with the size and position of the element, which you can use to calculate where the element should be positioned.
+
+Here's an example of what the object might look like for an element with a width of 100px, a height of 50px, and positioned at left: 200px and top: 100px:
+
+{
+  top: 100,
+  right: 300,
+  bottom: 150,
+  left: 200,
+  width: 100,
+  height: 50,
+  x: 200,
+  y: 100
+}
+*/
